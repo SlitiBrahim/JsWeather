c404bdf42f4bd46c8dcd1356780c2b2237fdbed0
SLITI Brahim <brahim.sliti@supinfo.com>
2017-11-25 17:18:01 +0100

    redisigning weather result display

--- weather.js
+++ weather.js
@@ -5,6 +5,7 @@ function displayResult(result) {
   document.querySelector("#temperature-details").innerText = "Pression: " + result.main.pressure + " pHa" + " - Humidité: " + result.main.humidity + " %.";
   document.querySelector('#description').innerText = result.weather[0].description.toUpperCase();
   document.querySelector("#wind").innerText = "Vitesse du vent: " + result.wind.speed + " km/hr";
+  document.querySelector("#weather-icon").src = "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png";
 }
 
 window.fetch("http://api.openweathermap.org/data/2.5/weather?q=Lyon&lang=fr&units=metric&appid=4d3796cbe39343750eedff5503a475c2")
