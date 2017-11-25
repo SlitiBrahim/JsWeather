176f814edc7cb3acb0a3915514216e269a9d126d
SLITI Brahim <brahim.sliti@supinfo.com>
2017-11-25 04:12:07 +0100

    Looking for Button Animation OK -> so all animations are Ok

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function fadeIn(element) {
+function fadeIn(element, isIn) {
 
   var op = 0.1;
 
@@ -16,9 +16,44 @@ function fadeIn(element) {
 
 }
 
+function scaleAnim(element, newValue, inAnim=true) {  // inAnim -> parameter to know if playing an mouseover anim
+
+  const defaultIncrementor = 0.01;
+  var incrementor = (inAnim ? defaultIncrementor : -defaultIncrementor);
+  var a = 0;
+
+  var timer = setInterval(function(){
+
+    a = 1+incrementor;
+
+    if ((inAnim && a >= newValue) || (!inAnim && a <= 1)) { // End if new Value is reached for inAnim, or end if default scale (1) is reached
+      clearInterval(timer);
+    }
+
+    scale(element, a);
+    incrementor += (inAnim ? defaultIncrementor : -defaultIncrementor);
+
+  }, 10);
+
+  if (!inAnim) { scale(element, 1); } // reset to default scale -> prevent non ending animation so get back to default scale
+
+}
+
+function scale(element, newValue) {
+  element.style.transform = "scale("+newValue+")";
+}
+
 function didLoad() {
   var contentTitle = document.querySelector(".content > #content-title");
-  fadeIn(contentTitle);
+  var lookforBtn = document.querySelector(".content > #lookfor-btn");
+
+  var overingLookForBtn;
+  var outingLookForBtn;
+  overingLookForBtn = lookforBtn.addEventListener("mouseover", scaleAnim.bind(true, lookforBtn, 1.04));
+  outingLookForBtn = lookforBtn.addEventListener("mouseout", scaleAnim.bind(true, lookforBtn, 1.04, false));
+
+  // fadeIn(contentTitle);
+  // setTimeout(fadeIn.bind(null, lookforBtn), 1200);
 }
 
 window.addEventListener("load", didLoad, true);
