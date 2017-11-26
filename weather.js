e1888af849fac8ce5bbf8edc7a44dc7b2951b63a
SLITI Brahim <brahim.sliti@supinfo.com>
2017-11-26 03:33:47 +0100

    Geolocation ok, so weather is now depending on user's localization if he accepted that

--- weather.js
+++ weather.js
@@ -1,13 +1,25 @@
 function displayResult(result) {
   console.log(result);
-  document.querySelector('#city').innerText = result.name;
-  document.querySelector('#temperature').innerText = "Actuellement: " + Math.round(result.main.temp) + 'C°' + " - Min: " + result.main.temp_min + " C°" + " - Max: " + result.main.temp_max + " C°";
-  document.querySelector("#temperature-details").innerText = "Pression: " + result.main.pressure + " pHa" + " - Humidité: " + result.main.humidity + " %.";
-  document.querySelector('#description').innerText = result.weather[0].description.toUpperCase();
-  document.querySelector("#wind").innerText = "Vitesse du vent: " + result.wind.speed + " km/hr";
+  document.querySelector('#city > span').innerText = result.name;
+  document.querySelector('#temperature > span').innerText = "Actuellement: " + Math.round(result.main.temp) + 'C°' + " - Min: " + result.main.temp_min + " C°" + " - Max: " + result.main.temp_max + " C°";
+  document.querySelector("#temperature-details > span").innerText = "Pression: " + result.main.pressure + " pHa" + " - Humidité: " + result.main.humidity + " %.";
+  document.querySelector('#description > span').innerText = result.weather[0].description.toUpperCase();
+  document.querySelector("#wind > span").innerText = "Vitesse du vent: " + result.wind.speed + " km/hr";
   document.querySelector("#weather-icon").src = "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png";
 }
 
-window.fetch("http://api.openweathermap.org/data/2.5/weather?q=Lyon&lang=fr&units=metric&appid=4d3796cbe39343750eedff5503a475c2")
-  .then(res => res.json())
-  .then(resJson => displayResult(resJson))
+navigator.geolocation.getCurrentPosition(success, error);
+
+function success(pos) {
+  console.log(pos);
+
+  window.fetch("http://api.openweathermap.org/data/2.5/weather?lat="+pos.coords.latitude+"&lon="+pos.coords.longitude+"&lang=fr&units=metric&appid=4d3796cbe39343750eedff5503a475c2")
+    .then(res => res.json())
+    .then(resJson => displayResult(resJson))
+
+
+}
+
+function error() {
+  console.log("Please allow geolocalisation to display weather.");
+}
